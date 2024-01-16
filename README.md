ee915ab057ca5dfd7d0bf21dcc304490dc4de04e
ojha111 <152144787+ojha111@users.noreply.github.com>
2024-01-16 11:51:05 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# txt uploader
+
+# Made By ٭🤍 ܔसत्यम शिवम सुन्दरम➼❤️
+
+
+## DEPLOY TO HEROKU
+
+
+[![Deploy to heroku chacha](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/Rahulsinghcreator/tiger-txt-ADVANCED)
---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+API_ID = API_ID = 21411989
+
+API_HASH = os.environ.get("API_HASH", "ba89c6036c699003a37a5bb0256d6ff6")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6458316836:AAF6xzi1SPkBgmPTUpytlDbxf3zG6Tf43dU")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 1358747363))
+
+LOG = -1002013062742
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "6687634412").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+ADMINS.append(OWNER)
+
+
